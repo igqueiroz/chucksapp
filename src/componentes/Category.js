d1ff8f85b192f41f7583d0aa45061d42d1b2ddcb
Igor Queiroz <andyarm@gmail.com>
2018-03-25 22:00:17 -0300

    Update Category.js

--- src/componentes/Category.js
+++ src/componentes/Category.js
@@ -14,16 +14,22 @@ export default class Category extends Component {
         progress: true
       };  
     }
-    componentWillMount(){
-    this.props.routes[0].store.subscribe(() => {
-        this.setState({categories:this.props.routes[0].store.getState(), progress: false});
-    })
-    this.props.routes[0].store.dispatch(JokesApi.listaCategorias(`https://api.chucknorris.io/jokes/categories`))
+    componentDidMount(){
+        this.props.routes[0].store.subscribe(() => {
+            if (this.refs.refcategory) {
+                this.setState({categories:this.props.routes[0].store.getState(), progress: false}, () =>
+                document.querySelectorAll('#wrapper-box li a').forEach (item => {
+                    setTimeout(() => { item.style.opacity = "1"}, 100);
+                })
+            );
+            }
+        })
+        this.props.routes[0].store.dispatch(JokesApi.listaCategorias(`https://api.chucknorris.io/jokes/categories`));
     }
 
     render(){
         return (
-            <div>  
+            <div ref="refcategory">  
                 <ReactCSSTransitionGroup
                   transitionName="jokes"
                   transitionEnterTimeout={500}
